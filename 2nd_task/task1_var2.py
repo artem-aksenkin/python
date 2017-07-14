a12901ca9622ed72abbe2de84d70f7b92aedf00a
artem-aksenkin <artemaksenkin@gmail.com>
2017-07-14 21:20:02 +0300

    3rd task_var2 commit

--- 2nd_task/task1_var2.py
+++ 2nd_task/task1_var2.py
@@ -3,9 +3,8 @@ b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 c = []
 i = 0
 while i <= len(b):
-    a.count(i)
-    i += 1
-    if i in a:
-        if i in b:
+    if a.count(i) in a:
+        if a.count(i) in b:
             c.append(i)
+    i += 1
 print(c)
